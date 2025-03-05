39f752bda9d85238f1850fcded2669a4d0a992e1
Twinkle Handa <twinklehandaa@gmail.com>
2025-03-05 11:23:22 +0530

    Color Channels

--- image_END.py
+++ image_END.py
@@ -1,8 +1,16 @@
 import numpy as np
 import cv2
-img = cv2.imread('../images/devon.jpg')
+img = cv2.imread('C:/Users/hp/Downloads/Ex_Files_Deep_Learning_OpenCV/Exercise Files/images/devon.jpg')
 #print(img.shape)
 
+b = img[:,:,0]
+print(b)
+g = img[:,:,1]
+r = img[:,:,2]
+
+cv2.imshow('Blue', b)
+cv2.imshow('Green', g)
+cv2.imshow('Red', r)
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
